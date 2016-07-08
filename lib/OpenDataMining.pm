96c273fb1bbd362d341c2d6265bd698352d5fb80
Andrey A. Konovalov <drvtiny@gmail.com>
2016-07-08 19:47:30 +0300

    /mosrides processing parallelized

--- lib/OpenDataMining.pm
+++ lib/OpenDataMining.pm
@@ -1,10 +1,11 @@
 package OpenDataMining;
 use utf8;
+use v5.14.1;
 use Data::Dumper qw(Dumper);
 use Redis;
 use LWP::Simple;
 use Mojo::Base 'Mojolicious';
-use JSON qw(encode_json decode_json);
+use JSON::XS qw(encode_json decode_json);
 use POSIX qw(strftime);
 use HTML::Entities;
 use lib '/opt/libs/Perl5';
@@ -12,13 +13,18 @@ use CSV::Lite qw(csvPrint);
 use Math::BigFloat ':constant';
 use Math::Round qw(nearest);
 use Date::Simple qw(date today);
+use AnyEvent::HTTP qw(http_get);
 use Mojolicious::Types;
 use IO::Compress::Gzip 'gzip';
+use IO::Uncompress::Gunzip 'gunzip';
 my $types = Mojolicious::Types->new;
 $types->type('csv' => 'text/csv', 'json'=>'application/json; charset=utf-8');
 #$app->types->type('csv' => 'text/csv');
 
 my %Methods=(
+  'testua'=>{
+    'cache'=>undef,
+  },
   'time'=>{
     'cache'=>undef,
   }, # <- time
@@ -78,8 +84,8 @@ my %Methods=(
   }, # <- mosforeca
   'mosrides'=>{
     'cache'=>{
-      'enabled'=>1,
-      'period'=>-1,
+      'enabled'=>0,
+      'period'=>600,
       'key'=>'MosRides',
     },
     'compression'=>{
@@ -87,7 +93,7 @@ my %Methods=(
       'type'=>'gzip',
     },
     'source'=>{
-      'url'=>'http://katushkin.ru/rides',
+      'url'=>'http://m.katushkin.ru/rides',
     },
     'limit'=>{'max_days'=>31},
   } # <- mosrides
@@ -100,6 +106,8 @@ sub startup {
   my $jsonData;
   my $flStopProcessing=0;
   my $log=$self->app->log;
+  $self->app->ua->proxy->detect();
+  $log->debug('Detected HTTP proxy: '.$self->app->ua->proxy->http) if $self->app->ua->proxy->http;
   # Documentation browser under "/perldoc"
   # $self->plugin('PODRenderer');
   $self->secrets(['Cr4bUtH@cK']);
@@ -118,6 +126,7 @@ sub startup {
         $c->render('json'=>{'error'=>'No such method'});
         return 0;
       }
+      $log->debug('In before_dispatch()');
       return 1 unless $met->{'cache'} and $met->{'cache'}{'enabled'};
       $log->debug('Method '.$slfName.' require caching');
       my $cache=$met->{'cache'};
@@ -181,7 +190,7 @@ sub startup {
       my $met=$Methods{$slfName};
             
       # Check if "gzip => 1" has been set in the stash
-      do { $log->debug('Compressiion not required'); return 1 }
+      do { $log->debug('Compression not required'); return 1 }
         unless $c->stash->{'gzip'} || (ref($met->{'compression'}) eq 'HASH' && $met->{'compression'}{'enabled'});
 
       # Check if user agent accepts GZip compression
@@ -200,7 +209,18 @@ sub startup {
     $log->debug('Request: ',$v->req->url);
     $v->render('json'=>['a','b','c']);
   });
-
+  
+  $r->get('/testua' => sub {
+    my $v=shift;
+#    $v->app->ua->proxy->http('http://msk1-vm-proxy01.unix.nspk.ru:3128');
+# encode_json([map {utf8::decode($_); $v->app->log->debug('header h1: '.$_); $_} @{$dom->find('h1')->map('text')->to_array}]),
+    my $dom=$v->app->ua->get('http://os2.ru')->res->body;
+    $v->render(
+      'text' => $dom,
+      'format' => 'html'      
+    );
+  });
+  
   $r->get('/time' => sub {
     my $v=shift;
     $v->render('text' => join('','{"time":',time(),'}'),'format'=>'json');
@@ -276,27 +296,28 @@ sub startup {
   $r->get('/mosrides' => sub {
     my $v=shift;
     (my $slfName = $v->req->url) =~ s%^/([^?]+)(?:\?.+)?$%$1%;
-    my $ua=LWP::UserAgent->new();
     my $config=$Methods{$slfName};
         
     my %hshRides;
-    my @skip;
     my $iSkip=0;
     my ($baseUrl,$srcUrl)=($config->{'source'}{'url'},undef);
     my ($siteUrl)=$baseUrl=~m%^\s*(https?://[^/]+)%i;
     my $maxDate=today()+$config->{'limit'}{'max_days'};
-    RIDEPAGE: while ($iSkip<=abs($#skip)) {
-      $srcUrl=$baseUrl.(@skip?'?skip='.$skip[$iSkip++]:'');
-#      $log->debug('get url="'.$srcUrl.'"');
-      my $htmlRides=$ua->get($srcUrl)->decoded_content;
-      @skip=sort { $a <=> $b } keys {map { $_=>1 } $htmlRides=~m%\?skip=(\d+)"%sg} unless @skip;
-      while ($htmlRides=~m/<a href="(\/rides\/(\d{4}\/\d{2}\/\d{2})\/\d+)"\s*>\s*([^<]+)\s*<\/a>.*?(\d+:\d+)[^<]*?<\/span>.+?<a\s+href="([^"]+)">Участвуют<\/a>.*?<span[^>]*>\s*(\d+)\s*<\/span>/sg) {
-        my ($relUrl,$YYYYMMDD,$rideTitle,$HHMM,$urlParts,$nParts)=($1,$2,$3,$4,$5,$6);
+    my $ua=$v->app->ua;
+    my @cmn;
+    my $doExtractRides=sub {
+      my ($refHtmlRides, $nSkip)=@_;
+      $nSkip||=0;
+      printf "In doExtractRides()\[%d\], htmlRides.length=%d\n", $nSkip, length($$refHtmlRides);
+      utf8::decode($$refHtmlRides);
+      while ($$refHtmlRides=~m/<a href="(\/rides\/(\d{4}\/\d{2}\/\d{2})\/\d+)"\s*>\s*([^<]+)\s*<\/a>.*?(\d+:\d+)[^<]*?<\/span>.+?<a\s+href="([^"]+)">Участвуют<\/a>.*?<span[^>]*>\s*(\d+)\s*<\/span>/sg) {
+        my ($relUrl,$YYYYMMDD,$rideTitle,$HHMM,$urlParts,$nParts)=($1,$2,$3,$4,$5,$6);        
         $YYYYMMDD=~tr/\//-/;
-        last RIDEPAGE if $YYYYMMDD gt $maxDate;
+#        last RIDEPAGE if $YYYYMMDD gt $maxDate;
         my $link=$siteUrl.$relUrl;
-        $hshRides{$link}=
-                    {
+        say '['.$nSkip.'] =====> '.$link;
+        next if exists $hshRides{$link};
+        $hshRides{$link} = {
                       'day'=>      $YYYYMMDD,
                       'time'=>     $HHMM,
                       'fullTime'=> $YYYYMMDD.' '.$HHMM,
@@ -306,13 +327,89 @@ sub startup {
                         'link' 	=> $urlParts=~m@^https?://@i?$urlParts:$siteUrl.$urlParts,
                         'n'	=> $nParts,
                       },
-                    } unless $hshRides{$link};
+        };
       }
+    };
+    my $renderRides=sub {
+      my $hr={'katushkin_rides'=>[ sort { $a->{'fullTime'} cmp $b->{'fullTime'} } values %hshRides ]};
+      $jsonData=JSON::XS->new->utf8->encode($hr);
+      $v->render('json'=>$hr);
+    };
+    # Get first page in usual "blocking" mode:
+    my $htmlRides=$ua->max_redirects(5)->get($baseUrl)->res->body;
+    open F,'>','/tmp/rides.html';
+    print F $htmlRides;
+    close(F);
+    my @skip=sort { $a <=> $b } keys {map { $_=>1 } $htmlRides=~m%\?skip=(\d+)"%sg};
+    $doExtractRides->(\$htmlRides);
+    $log->debug('Ride pages: '.join(','=>@skip));
+    my $nSkip;
+    unless ($v->param('native')) {
+      my $aeCondVar=AnyEvent->condvar;
+      for my $ridesPageUrl (map $baseUrl.'/?skip='.$_, grep {$_<=45} @skip) {
+        $log->debug('Trying to fetch URL (with AnyEvent): '.$ridesPageUrl); 
+        $aeCondVar->begin;
+        http_get $ridesPageUrl, 'headers'=>{'Accept-Encoding'=>'gzip, deflate'}, sub {
+          my ($body,$headers)=@_;
+          
+          unless (substr($headers->{'Status'},0,1) eq '2') {
+            $log->debug(sprintf 'Error: cant fetch URL <<%s>>. Status code: %d', $ridesPageUrl, $headers->{'Status'});
+            $aeCondVar->end;
+            return undef;
+          } 
+          
+          my $flHTMLZiped=exists($headers->{'Content-Encoding'}) and $headers->{'Content-Encoding'}=~m/gzip/i;
+          $log->debug('Data succesfully retrieved from: '.$ridesPageUrl.($flHTMLZiped?'. Content was compressed.':''));          
+          my $refHtml=$flHTMLZiped?do {
+                                    gunzip \$body => \my $uncomp;
+                                    \$uncomp
+                                   }
+                                  :\$body;          
+          if ($nSkip=($ridesPageUrl=~m%skip=(\d+)%)[0]) {
+            push @cmn, $nSkip;
+            my $tmp="/tmp/rides__${nSkip}.html";
+            open my $fh,'>',$tmp;
+            say $fh $$refHtml;
+            close $fh;
+            $log->debug("HTML was saved to the file: $tmp");
+          }
+          $doExtractRides->($refHtml,$nSkip);
+          $aeCondVar->end;
+        };
+      }
+      $aeCondVar->recv;
+      $log->debug(Dumper \@cmn);
+      $renderRides->();
+    } else {
+       $v->render_later;
+      my $delay = Mojo::IOLoop->delay(sub {
+        $log->debug('Delay finished!');
+        $log->debug(Dumper \@cmn);
+        $renderRides->();
+      });
+      for my $ridesPageUrl (map $baseUrl.'/?skip='.$_, grep {$_<=45} @skip) {
+        $log->debug('Trying to fetch URL: '.$ridesPageUrl);
+        $ua->cookie_jar->ignore(sub {1});
+        my $end=$delay->begin;
+        $ua->max_redirects(5)->get($ridesPageUrl, sub {
+          my ($userAgent,$rqResult)=@_;
+          $log->debug("Yeah, my url is $ridesPageUrl");
+          my $html=$rqResult->res->body;
+          if ($nSkip=($ridesPageUrl=~m%skip=(\d+)%)[0]) {
+            push @cmn, $nSkip;
+            my $tmp="/tmp/rides__${nSkip}.html";
+            open my $fh,'>',$tmp;
+            say $fh $html;
+            close $fh;
+            $log->debug("HTML was saved to the file: $tmp");
+          }
+          $doExtractRides->(\$html,$nSkip);
+          $end->();
+        });
+      }
+#      Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
     }
-    my $hr={'katushkin_rides'=>[ sort { $a->{'fullTime'} cmp $b->{'fullTime'} } values %hshRides ]};
-    $jsonData=encode_json($hr);
-    $v->render('json'=>$hr);
-  });
+  }); # <- /mosrides
   
   $r->get('/status' => sub {
     my $v=shift;
@@ -323,14 +420,17 @@ sub startup {
           'date'=>'2015/10/01',
           'target_version'=>'0.01',
         },
-        'params'=>$v->req->params->to_hash,
+        'params'=>$v->req->params->to_hash,        
         'methods'=>[keys %Methods],
+        'env'=>\%ENV,
       });
-  });
+  }); # <- /status
   
   $r->get('/euronews' => sub {
     my $v=shift;
     my $ua = LWP::UserAgent->new;
+    $ua->env_proxy;
+  #  my $rss = $v->app->ua->get('http://feeds.feedburner.com/euronews/ru/home/')->res->body;
     my $rss = $ua->get('http://feeds.feedburner.com/euronews/ru/home/')->decoded_content;
   #  return $v->render('text'=>$rss, format=>'txt');
     my @news = $rss =~ m%<title>(.+?)</title>%sg;
